6ac602c533f1a709051c3703f108ea4826bb1908
iverly <github@iverly.net>
2022-05-16 20:33:06 +0200

    feat: add AppBar with icons, menus & responsive
    Signed-off-by: iverly <github@iverly.net>

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,18 +1,145 @@
-import React from 'react';
+import * as React from 'react';
 import MAppBar from '@mui/material/AppBar';
-import CodeIcon from '@mui/icons-material/Code';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import CodeIcon from '@mui/icons-material/Code';
+
+const pages = ['Practice', 'Certification', 'Challenges'];
+const settings = ['Profile', 'Settings', 'Logout'];
 
 export default function AppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <MAppBar position="relative">
-      <Toolbar>
-        <CodeIcon sx={{ mr: 2 }} />
-        <Typography variant="h6" color="inherit" noWrap>
-          PolyCode
-        </Typography>
-      </Toolbar>
+    <MAppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
+          <Box sx={{ display: { xs: 'none', md: 'flex' }, flexDirection: 'row', alignItems: 'center' }}>
+            <CodeIcon sx={{ mr: 1 }} />
+            <Typography
+              variant="h6"
+              color="inherit"
+              noWrap
+              component="div"
+            >
+              PolyCode
+            </Typography>
+
+          </Box>
+
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center" sx={{ mr: 2 }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' }, flexDirection: 'row', alignItems: 'center' }}>
+            <CodeIcon sx={{ mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+            >
+              PolyCode
+            </Typography>
+          </Box>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2, color: 'white', display: 'block', mr: 2,
+                }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box>
+            <Tooltip title="Click to open">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
     </MAppBar>
   );
 }
